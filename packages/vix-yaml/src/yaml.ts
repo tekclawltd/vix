9b7949d5cbdb8d2c07178fe0c7d21142cac8f24b
JoeyEtamity <etamity@JoeyEtamitysMBP.cust.communityfibre.co.uk>
2024-06-16 13:17:22 +0100

    update package

--- packages/vix-yaml/src/yaml.ts
+++ packages/vix-yaml/src/yaml.ts
@@ -1,13 +1,27 @@
 import yaml from 'js-yaml';
+import XRegExp from 'xregexp';
 import unsafe from './js-types';
 
 const schema = yaml.DEFAULT_SCHEMA.extend(unsafe);
 
-export default {
-  load: (src: string) => {
-    return yaml.load(src, { schema });
+const noTemplate = (src: string) =>
+  XRegExp.matchRecursive(src, '#js::begin', '#js::end', 'g').reduce(
+    (root: string, next: string) => {
+      return root?.replace(next, '');
+    },
+    src
+  );
+
+export const jsYaml = {
+  load: (src: string, noTemplateLiterals: boolean = false) => {
+    const noJsYaml = noTemplateLiterals ? noTemplate(src) : src;
+    return yaml.load(noJsYaml, { schema });
   },
-  loadAll: (src: string) => {
-    return yaml.loadAll(src, function (doc) {}, { schema });
+  loadAll: (src: string, noTemplateLiterals: boolean = false) => {
+    const noJsYaml = noTemplateLiterals ? noTemplate(src) : src;
+    return yaml.loadAll(noJsYaml, function (doc) {}, { schema });
   },
+  dump: yaml.dump,
 };
+
+export default jsYaml;
